1492f4fd1b2b8fb5c5b78f5700f62bdb9f824d65
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-28 13:25:28 +0100

    gts210ltexx: Add mixer config for new audio HAL
    This is a simplified configuration, but it works well
    enough.
    
    Tested usecases:
    
    [+] speaker output (media playback)
    [+] mic input (voice recording)
    [+] mic input (voice recognition)
    
    Change-Id: Ife589adfb02e083039e2a614b14aa9aeb6f77b77

--- device.mk
+++ device.mk
@@ -6,7 +6,8 @@ PRODUCT_PACKAGES += \
 
 # Tinyalsa
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
