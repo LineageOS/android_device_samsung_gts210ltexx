fa7f92d1524b5fb9ceeeb808f426deca73e8458a
Christopher N. Hesse <raymanfx@gmail.com>
2018-03-06 11:54:32 +0100

    gts210ltexx: cm -> lineage
    Change-Id: I05413a1c211455c3cbb1de303a45ac07c9d29b08

--- lineage.mk
+++ lineage.mk
@@ -5,8 +5,8 @@ $(call inherit-product, device/samsung/gts2-common/device-common.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_gts210ltexx
