283ce43928c030c6f5a91937360f8a8f6ba2a52b
MSe <mse1969@posteo.de>
2019-01-02 20:07:12 +0100

    Revert "Move mixer_paths_0.xml to common device tree"
    This reverts commit e9c728a199a924359f2b17a87d0ffba815a5e122.
    Reason: Breaks audio & video on gts210ltexx.
    
    Change-Id: I97ce125e02658b339d9a3ffbd462c5772d3f167c

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@ LOCAL_PATH := device/samsung/gts210ltexx
 PRODUCT_PACKAGES += \
     init.baseband.rc
 
+# Tinyalsa
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
